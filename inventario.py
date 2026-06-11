82dc3bcdeec985572c805b3695869b3eb3f217ab
RafaelPacheco <rafarafiti19@gmail.com>
2026-06-11 19:58:07 -0300

    Se habilito la opcion para cargar articulos a la base de datos

--- inventario.py
+++ inventario.py
@@ -1,3 +1,4 @@
+import sqlite3
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
@@ -13,6 +14,11 @@ class Inventario(tk.Frame):
         super().__init__(padre)
         self.controlador = controlador
         self.widgets()
+        self.articulos_combobox()
+        #aqui si no esta creada la carpeta fotos le pedimos al sistema que la cree
+        self.image_folder = "fotos"
+        if not os.path.exists(self.image_folder):
+            os.makedirs(self.image_folder)
         
         
     def widgets(self):
@@ -81,10 +87,10 @@ class Inventario(tk.Frame):
 
     #Creamos una funcion para cargar las imagenes
     def load_image(self):
-        file_path = filedialog.askopenfile()
+        file_path = filedialog.askopenfilename()
         if file_path :
             image = Image.open(file_path)
-            image = image.resize((200,200)), Image.LANCZOS
+            image = image.resize((200,200), Image.LANCZOS)
             image_name = os.path.basename(file_path)
             image_save_path = os.path.join(self.image_folder, image_name)
             image.save(image_save_path)
@@ -94,6 +100,13 @@ class Inventario(tk.Frame):
 
             img_label = tk.Label(self.frameimg, image = self.image_tk)
             img_label.place(x=0, y=0, width=200, height=200)
+
+    def articulos_combobox(self):
+        self.con = sqlite3.connect('database.db')
+        self.cur = self.con.cursor()
+        self.cur.execute("SELECT articulo FROM articulos")
+        self.articulos = [row[0] for row in self.cur.fetchall()]
+        self.comboboxbuscar['values'] = self.articulos
 
     #creamos una funcion para cargar articulos
     def agregar_articulo(self):
@@ -109,6 +122,68 @@ class Inventario(tk.Frame):
         top.focus_set()
         top.lift()
 
-        tk.Label(top, text="Articulos", font="arial 12 bold",bg="#FFE4E0").place(x=20,y=80,height=25)
+        tk.Label(top, text="Articulos:", font="arial 12 bold",bg="#FF7E63").place(x=20,y=20,height=25)
+        entry_articulo = ttk.Entry(top, font="arial 12 bold")
+        entry_articulo.place(x=120,y=20, width=250, height=30)
+
+        tk.Label(top, text="Precio: ", font="arial 12 bold", bg="#FF7E63").place(x=20, y=60, width=80, height=25)
+        entry_precio = ttk.Entry(top, font="arial 12 bold")
+        entry_precio.place(x=120, y=60, width=250, height=30)
+
+        tk.Label(top, text="Costo: ", font="arial 12 bold", bg="#FF7E63").place(x=20, y=100, width=80, height=25)
+        entry_costo = ttk.Entry(top, font="arial 12 bold")
+        entry_costo.place(x=120, y=100, width=250, height=30)
+
+        tk.Label(top, text="Stock: ", font="arial 12 bold", bg="#FF7E63").place(x=20, y=140, width=80, height=25)
+        entry_stock = ttk.Entry(top, font="arial 12 bold")
+        entry_stock.place(x=120, y=140, width=250, height=30)
+
+        tk.Label(top, text="Estado: ", font="arial 12 bold", bg="#FF7E63").place(x=20, y=180, width=80, height=25)
+        entry_estado = ttk.Entry(top, font="arial 12 bold")
+        entry_estado.place(x=120, y=180, width=250, height=30)
+
+        self.frameimg = tk.Frame(top, bg="white",highlightbackground="gray",highlightthickness=1)
+        self.frameimg.place(x=440, y=30, width=200, height=200)
+
+        btnimage = tk.Button(top, text="Cargar Imagen", font="arial 12 bold",command=self.load_image)
+        btnimage.place(x=470,y=260,width=150,height=40)
+
+        #Creamos una funcion dentro de la funcion agregar articulos para guardar
+        def guardar():
+            articulo = entry_articulo.get()
+            precio = entry_precio.get()
+            costo = entry_costo.get()
+            stock = entry_stock.get()
+            estado = entry_estado.get()
+
+
+            if not articulo or not precio or not costo or not stock or not estado:
+                messagebox.showerror("Error", "Todos los campos deben ser completados")
+                return
+            
+            try:
+                precio = float(precio)
+                costo = float(costo)
+                stock = int(stock)
+            except ValueError:
+                messagebox.showerror("Error", "Precio, costo y stock deben ser numeros validos")
+                return
+            if hasattr(self, 'image_path'):
+                image_path = self.image_path
+                #Foto que aparecera por default si no cargamos ninguna foto en producto
+            else:
+                image_path = (r"fotos/default.jpeg")
+            
+            try:
+                self.cur.execute("INSERT INTO articulos(articulo, precio, costo, stock, estado, image_path) VALUES(?,?,?,?,?,?)",
+                                 (articulo, precio, costo, stock, estado, image_path))
+                self.con.commit()
+                messagebox.showinfo("Exito","Articulo agregado exitosamente")
+                top.destroy()
+            except sqlite3.Error as e:
+                print("Error al cargar el articulo:", e)
+                messagebox.showerror("Error", "Error al agregar el articulo")
+        tk.Button(top,text="Guardar",font="arial 12 bold", command=guardar).place(x=50,y=260,width=150,height=40)
+        tk.Button(top,text="Cancelar",font="arial 12 bold", command=top.destroy).place(x=260,y=260,width=150,height=40)
 
 
